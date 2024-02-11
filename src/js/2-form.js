8917fb4bbbf7998a8d3e51dbcfb86495e2cc558c
Exxin <exxinblood@gmail.com>
2024-02-11 20:57:58 +0200

    а

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -3,26 +3,68 @@ import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const loginForm = document.querySelector('.login-form');
 
-  loginForm.addEventListener('submit', function (event) {
-    event.preventDefault();
+function isLocalStorageSupported() {
+  try {
+    const testKey = '__test__';
+    localStorage.setItem(testKey, testKey);
+    localStorage.removeItem(testKey);
+    return true;
+  } catch (e) {
+    return false;
+  }
+}
 
-    const emailInput = this.elements.email;
-    const passwordInput = this.elements.password;
+function saveFormDataToLocalStorage(formData) {
+  if (isLocalStorageSupported()) {
+    localStorage.setItem('formData', JSON.stringify(formData));
+  } else {
+    console.error('Local storage is not supported in this browser.');
+  }
+}
 
-    const emailValue = emailInput.value.trim();
-    const passwordValue = passwordInput.value.trim();
+function getFormDataFromLocalStorage() {
+  if (isLocalStorageSupported()) {
+    const formDataJSON = localStorage.getItem('formData');
+    return formDataJSON ? JSON.parse(formDataJSON) : null;
+  } else {
+    console.error('Local storage is not supported in this browser.');
+    return null;
+  }
+}
 
-    if (!emailValue || !passwordValue) {
-      alert('All form fields must be filled in');
-      return;
-    }
+window.addEventListener('DOMContentLoaded', function () {
+  const savedFormData = getFormDataFromLocalStorage();
+  if (savedFormData) {
+    const emailInput = loginForm.elements.email;
+    const passwordInput = loginForm.elements.password;
 
-    const formData = {
-      email: emailValue,
-      password: passwordValue
-    };
+    emailInput.value = savedFormData.email;
+    passwordInput.value = savedFormData.password;
+  }
+});
 
-    console.log(formData);
+loginForm.addEventListener('submit', function (event) {
+  event.preventDefault();
 
-    this.reset();
-  });
+  const emailInput = this.elements.email;
+  const passwordInput = this.elements.password;
+
+  const emailValue = emailInput.value.trim();
+  const passwordValue = passwordInput.value.trim();
+
+  if (!emailValue || !passwordValue) {
+    alert('All form fields must be filled in');
+    return;
+  }
+
+  const formData = {
+    email: emailValue,
+    password: passwordValue
+  };
+
+  console.log(formData);
+
+  saveFormDataToLocalStorage(formData);
+
+  this.reset();
+});
